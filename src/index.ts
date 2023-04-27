b280684d63b33f7c78e5de3cdcdf48cd39aa54ef
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-04-27 17:27:59 +0200

    chore: add source

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { LLama } from "llama-node";
+import { LLamaCpp, LoadConfig } from "llama-node/dist/llm/llama-cpp.js";
+import path from "path";
+
+const model = path.resolve(process.cwd(), "./ggml-model-q4_0.bin");
+
+const llama = new LLama(LLamaCpp);
+
+const config: LoadConfig = {
+    path: model,
+    enableLogging: true,
+    nCtx: 1024,
+    nParts: -1,
+    seed: 0,
+    f16Kv: false,
+    logitsAll: false,
+    vocabOnly: false,
+    useMlock: false,
+    embedding: false,
+    useMmap: true,
+};
+
+llama.load(config);
+
+const template = `How are you`;
+
+const prompt = `### Human:
+
+${template}
+
+### Assistant:`;
+
+llama.createCompletion(
+    {
+        nThreads: 4,
+        nTokPredict: 2048,
+        topK: 40,
+        topP: 0.1,
+        temp: 0.2,
+        repeatPenalty: 1,
+        stopSequence: "### Human",
+        prompt,
+    },
+    (response) => {
+        process.stdout.write(response.token);
+    }
+);

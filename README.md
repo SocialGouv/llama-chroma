45ba859e8e89bfe6c4019720497c612f29c44c56
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-04-27 22:31:11 +0200

    chore: cleaning

--- README.md
+++ README.md
@@ -1,29 +1,37 @@
 ## Development
 
-### Data directory
+### Set up
+
+#### Environment variables
+```shell
+DIRECTORY_PATH=./data
+LLAMA_MODEL=./model.bin
+```
+
+#### Data directory
 Add markdown files to a folder named `data`
 ```shell
 mkdir data
 cp /whatever/the/markdown/file/you/want/to.md ./data/
 ```
 
-### Model file
+#### Model file
 Add the embedding Llama model as a `model.bin` file
 ```shell
 cp /whatever/the/llama/model/you/want/to.bin ./model.bin
 ```
 
-### Build server
+#### Build server
 ```shell
 yarn build
 ```
 
-### Start server
+#### Start server
 ```shell
 yarn start
 ```
 
-### Start Chroma
+#### Start Chroma
 ```shell
 git clone git@github.com:chroma-core/chroma.git
 cd chroma
---
+++ src/api/indexing.ts
@@ -0,0 +1,59 @@
+import * as fs from "fs"
+import { ChromaClient } from "chromadb"
+import type { FastifyRequest, FastifyReply } from "fastify"
+
+import { getEmbeddings } from "../lib/llama"
+import markdownSplitter from "../lib/markdown-splitter"
+import readDirectoryRecursive from "../lib/read-directory-recursive"
+
+const client = new ChromaClient()
+
+async function addFileToCollection(filePath: string) {
+  const collection = await client.getOrCreateCollection("test-collection")
+  const content = fs.readFileSync(filePath).toString()
+  const chunks = markdownSplitter(content)
+  const embeddings = await Promise.all(
+    chunks.map((chunk) => getEmbeddings(chunk.content))
+  )
+
+  try {
+    await collection.add(
+      chunks.map((_file, i) => `${filePath}-${i}`),
+      embeddings,
+      chunks.map((file) => ({ path: filePath, type: file.type })),
+      chunks.map((file) => file.content)
+    )
+  } catch (e) {
+    console.log("error posting to chroma", e)
+  }
+
+  console.info(`${filePath}: added ${chunks.length} chunks`)
+  return {
+    content,
+    chunks,
+    path: filePath,
+  }
+}
+
+async function addFilesToCollection(files: string[]) {
+  for (const file of files) {
+    await addFileToCollection(file)
+  }
+}
+
+function getFiles() {
+  if (process.env.DIRECTORY_PATH) {
+    return readDirectoryRecursive(process.env.DIRECTORY_PATH)
+  }
+  throw new Error("process.env.DIRECTORY_PATH is undefined")
+}
+
+export default async function Indexing(
+  _request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const files = await getFiles()
+  console.log("files", files)
+  await addFilesToCollection(files)
+  reply.send({ indexedFiles: files.length })
+}
